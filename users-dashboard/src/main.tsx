0b8c36e2d800d8accfa9c5e98567ac0a9809ff0f
shinigami <157151797+shinigamich@users.noreply.github.com>
2026-05-07 23:50:01 +0300

    adding proj structure + providers

--- users-dashboard/src/main.tsx
+++ users-dashboard/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './app/providers/queryClient'
+import App from './app/App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
+  </QueryClientProvider>
 )
